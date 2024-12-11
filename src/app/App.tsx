bb739de7e03ddc6b8728c50c81a10ec7bddf79ad
ektto1041 <dhkdwk1041@gmail.com>
2024-12-11 14:13:38 +0900

    feat: add Wrapper and Container to App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import styles from "./App.module.css";
+import "./style/global.css";
 
 export default function App() {
   return (
-    <>
-      <Outlet />
-    </>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <Outlet />
+      </div>
+    </div>
   );
 }
