074bad4fbf52d627334eb50b19659d8ebffa3dc0
ektto1041 <dhkdwk1041@gmail.com>
2024-12-11 17:31:23 +0900

    feat: MainInput value, maxLength, onChange 추가

--- src/shared/main-input/ui/MainInput.tsx
+++ src/shared/main-input/ui/MainInput.tsx
@@ -1,23 +1,36 @@
-import { HTMLInputTypeAttribute } from "react";
+import type { ChangeEventHandler, HTMLInputTypeAttribute } from "react";
 import styles from "./MainInput.module.css";
 
 interface MainInputProps {
   icon: string;
   placeholder?: string;
   type?: HTMLInputTypeAttribute;
+  maxLength?: number;
+  value: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
 export default function MainInput({
   icon,
   placeholder = "",
   type = "text",
+  maxLength,
+  value,
+  onChange,
 }: MainInputProps) {
   return (
     <div className={styles.container}>
       <div className={styles["icon-wrapper"]}>
         <img src={icon} alt={icon} />
       </div>
-      <input className={`text-md`} placeholder={placeholder} type={type} />
+      <input
+        maxLength={maxLength}
+        className={`text-md`}
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 }
