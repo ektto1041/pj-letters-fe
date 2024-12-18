baa38dd92c80cec2769802a2121e7e355552444c
Sangyeon Park <53224839+ektto1041@users.noreply.github.com>
2024-12-18 16:28:46 +0900

    [PJL-014] CardPage API (#53)
    * chore: LetterItem type 추가
    
    * feat: GetLetter API

--- src/features/letter/api/LetterApi.ts
+++ src/features/letter/api/LetterApi.ts
@@ -1,8 +1,14 @@
 import { authAxios } from "@/shared";
-import { NewLetter } from "../model/types";
+import { LetterItem, NewLetter } from "../model/types";
 
 export const createLetter = async (newLetter: NewLetter) => {
   const response = await authAxios.post<unknown>(`api/letter/write`, newLetter);
 
   return response.data;
+};
+
+export const getLetterById = async (letterId: number) => {
+  const reponse = await authAxios.get<LetterItem>(`api/letter/${letterId}`);
+
+  return reponse.data;
 };
--- src/features/letter/index.ts
+++ src/features/letter/index.ts
@@ -1,4 +1,4 @@
-export type { Letter, NewLetter } from "./model/types";
+export type { Letter, NewLetter, LetterItem } from "./model/types";
 export { cardImgs, cardGrayImgs } from "./model/LetterImg";
 export { default as LetterBase } from "./ui/LetterBase";
-export { createLetter } from "./api/LetterApi";
+export { createLetter, getLetterById } from "./api/LetterApi";
